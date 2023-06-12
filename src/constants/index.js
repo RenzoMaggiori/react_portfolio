7e6c39a9cbfbe23a07f871a3ec016f9ea2ef829a
RenzoMaggiori <renzo.maggiori@epitech.eu>
2023-06-12 21:54:53 +0200

    [add] first commit

---
+++ src/constants/index.js
@@ -0,0 +1,138 @@
+import { Star } from "../assets";
+
+export const navLinks = [
+    {
+      id: "About me",
+      title: "About me",
+    },
+    {
+      id: "Skills",
+      title: "Skills",
+    },
+    {
+      id: "Proyects",
+      title: "Proyects",
+    },
+    {
+      id: "Contact",
+      title: "Contact",
+    },
+];
+
+export const stats = [
+  {
+    id: "stats-1",
+    title: "C Programing",
+    value: "+1000h",
+  },
+  {
+    id: "stats-2",
+    title: "Javascript",
+    value: "+500h",
+  },
+  {
+    id: "stats-3",
+    title: "Python",
+    value: "+300h",
+  },
+  {
+    id: "stats-4",
+    title: "C# Programing",
+    value: "+100h",
+  },
+  {
+    id: "stats-5",
+    title: "React",
+    value: "+400h",
+  },
+  {
+    id: "stats-6",
+    title: "Tailwind css",
+    value: "+300h",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Useful Links",
+    links: [
+      {
+        name: "LinkedIn",
+        link: "https://www.linkedin.com/in/renzomaggiori//",
+      },
+      {
+        name: "Github",
+        link: "https://github.com/RenzoMaggiori",
+      },
+      {
+        name: "Portfolio",
+        link: "Home",
+      },
+    ],
+  },
+  {
+    title: "Contact me",
+    links: [
+      {
+        name: "+54 911 5752-5636",
+        link: "",
+      },
+      {
+        name: "maggiorirenzo@gmail.com",
+        link: "",
+      },
+      {
+        name: "Suggestions",
+        link: "https://www.hoobank.com/suggestions/",
+      },
+      {
+        name: "Blog",
+        link: "https://www.hoobank.com/blog/",
+      },
+      {
+        name: "Newsletters",
+        link: "https://www.hoobank.com/newsletters/",
+      },
+    ],
+  },
+  {
+    title: "Partner",
+    links: [
+      {
+        name: "Our Partner",
+        link: "https://www.hoobank.com/our-partner/",
+      },
+      {
+        name: "Become a Partner",
+        link: "https://www.hoobank.com/become-a-partner/",
+      },
+    ],
+  },
+];
+
+export const features = [
+  {
+    id: "feature-1",
+    icon: Star,
+    title: "42sh",
+    content:
+      "A shell created in C which includes all the basic functions as well as some bonuses.",
+    link: "https://github.com/RenzoMaggiori/42sh",
+  },
+  {
+    id: "feature-2",
+    icon: Star,
+    title: "Lemin",
+    content:
+      "An ant based calculation unit that finds the way out of the nest for the ants.",
+    link: "https://github.com/RenzoMaggiori/Lemin",
+  },
+  {
+    id: "feature-3",
+    icon: Star,
+    title: "To Do app",
+    content:
+      "A proyect based on building an API with javascript, mySQL and node.js to create a To Do App",
+    link: "https://github.com/RenzoMaggiori/To-do"
+  },
+];
